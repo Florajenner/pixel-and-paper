a53337ed7ab1b68d90038d6dfb6c0186c57d33d6
Flora Jenner <flora.jenner@gmail.com>
2024-10-26 18:42:42 +0000

    add product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    file = models.FileField(upload_to='downloads/')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
