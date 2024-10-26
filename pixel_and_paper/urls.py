074af1084c0137cfb3da8c4abfbfbea24f374b23
Flora Jenner <flora.jenner@gmail.com>
2024-10-26 18:54:48 +0000

    add urls.py for products

--- pixel_and_paper/urls.py
+++ pixel_and_paper/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('products/', include('products.urls')),
     path('admin/', admin.site.urls),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'products/product_detail.html', {'product': product})
