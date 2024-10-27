9c5064932f10ffb0cee53dd7838b22cc02fe8c20
Flora Jenner <flora.jenner@gmail.com>
2024-10-27 19:24:31 +0000

    Display product images on the homepage product list and product detail pages.

--- products/models.py
+++ products/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
-from django.db import models
-
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    file = models.FileField(upload_to='downloads/')
+    file = models.FileField(upload_to='product_images')
     slug = models.SlugField(unique=True)
+    category = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+
+
