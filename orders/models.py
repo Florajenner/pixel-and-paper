04fc75cfe58d9799a914a11009ee6bc8626e9cc0
Flora Jenner <flora.jenner@gmail.com>
2024-10-27 13:22:32 +0000

    Add order model with 'is_paid' field and make migrations for orders app

--- orders/models.py
+++ orders/models.py
@@ -1,14 +1,12 @@
 from django.db import models
-
-from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    date_ordered = models.DateTimeField(auto_now_add=True)
-    paid = models.BooleanField(default=False)
+    order_date = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Order {self.id} by {self.user.username}"
+        return f"Order {self.id} - {self.user.username}"
