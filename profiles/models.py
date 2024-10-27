c543eda519791b886b1976af1b4e01512a4219cc
Flora Jenner <flora.jenner@gmail.com>
2024-10-27 13:19:47 +0000

    fix profiles model

--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-
-from django.db import models
 from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.user.username}'s profile"
+        return f"{self.user.username}'s Profile"
