345af409d265dc4e323e67b566bf8a0711c37ca3
FLORA JENNER <flora.jenner@gmail.com>
2024-11-22 17:20:26 +0000

    correct bug in product model and edit logout font in header

--- products/models.py
+++ products/models.py
@@ -1,16 +1,19 @@
 from django.db import models
-
 class Product(models.Model):
+    CATEGORIES = [
+        ('abstract', 'Abstract Art'),
+        ('nature', 'Nature & Landscapes'), 
+        ('typography', 'Typography'),
+        ('minimalist', 'Minimalist'),
+        ('geometric', 'Geometric')
+    ]
+    
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    file = models.FileField(upload_to='product_images')
+    preview_image = models.ImageField(upload_to='preview_images/')
+    downloadable_file = models.FileField(upload_to='digital_downloads/')
     slug = models.SlugField(unique=True)
-    category = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
-
-    def __str__(self):
-        return self.title
-
-
-
+    category = models.CharField(max_length=100, choices=CATEGORIES)
+    dimensions = models.CharField(max_length=100)
+    file_format = models.CharField(max_length=50)
